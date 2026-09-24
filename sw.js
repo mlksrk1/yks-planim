5fa77a02f5aba63afb21bb0d34ebad4f95b16081
mlksrk1 <mlksrk1@users.noreply.github.com>
2026-09-24 13:26:39 +0300

    Android ve iOS web uygulamasi destegi ekle

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='yks-planim-v1';
+const FILES=['./','./index.html','./styles.css','./setup.css','./day-status.css','./badges.css','./app.js','./gizlilik.html','./icon.svg','./manifest.webmanifest'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))))});
