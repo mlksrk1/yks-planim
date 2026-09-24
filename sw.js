4a9010a4de367e6fb859f2f61b49d46ba9626515
mlksrk1 <mlksrk1@users.noreply.github.com>
2026-09-24 13:43:41 +0300

    Ayarlar dugmesini tarih ve gorevler arasina tasi

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yks-planim-v8';
+const CACHE='yks-planim-v9';
 const FILES=['./','./index.html','./styles.css','./setup.css','./day-status.css','./badges.css','./app.js','./gizlilik.html','./icon.svg','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
